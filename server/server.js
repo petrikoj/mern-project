622df05c755f0ca67d4a28724a97f12b4a3330ff
petrikoj <100421329+petrikoj@users.noreply.github.com>
2022-09-21 17:55:32 +0200

    Connecting to MongoDB

--- server/server.js
+++ server/server.js
@@ -1,13 +1,12 @@
 import express from "express";
 import cors from "cors";
+import mongoose from "mongoose";
+import router from "./routes/playlists.js";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
-
-const port = process.env.PORT || 5000;
-
-app.listen(port, () => {
-  console.log("Server is running on port " + port);
-});
 
 app.use(express.json());
 app.use(
@@ -17,3 +16,21 @@ app.use(
 );
 
 app.use(cors());
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log("Listening on port " + port);
+});
+
+const connectToDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+connectToDB();
+
+app.use("/api", router);
