8d0f4c982fca6f678ada8024a0ba814e06d024af
petrikoj <100421329+petrikoj@users.noreply.github.com>
2022-10-04 17:40:37 +0200

    Fixing signup issue on Mongo DB side

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -3,14 +3,14 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
   username: {
     type: String,
-    required: true,
-    unique: true,
+    // required: true,
+    // unique: true,
   },
   email: {
     type: String,
     required: true,
     lowercase: true,
-    unique: true,
+    // unique: true,
     // match: /.+\@.+\..+/,
     // validate: (value) => {
     //   return validator.isEmail(value);
@@ -21,7 +21,7 @@ const userSchema = new mongoose.Schema({
   },
   password: {
     type: String,
-    required: true,
+    // required: true,
   },
 
   playlists: [{ type: mongoose.Schema.Types.ObjectId, ref: "playlist" }],
