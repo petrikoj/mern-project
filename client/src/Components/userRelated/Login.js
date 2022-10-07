3bedd9ad4db9f669f922031bab4ba10421f41a34
petrikoj <100421329+petrikoj@users.noreply.github.com>
2022-10-07 18:02:07 +0200

    Basic Email validation working

--- client/src/Components/userRelated/Login.js
+++ client/src/Components/userRelated/Login.js
@@ -1,8 +1,12 @@
-import { React, useState } from "react";
+import { React, useState, useEffect } from "react";
 import { Box, Container } from "@chakra-ui/react";
+import { isValidEmail, isValidPassword } from "../../utils/validators.js";
 
 function Login() {
   const [userLogin, setUserLogin] = useState({});
+  const [emailError, setEmailError] = useState(null);
+  const [passwordError, setPasswordError] = useState(null);
+  const [credentialsError, setCredentialsError] = useState(null);
 
   const handleChangeHandler = (event) => {
     setUserLogin({ ...userLogin, [event.target.name]: event.target.value });
@@ -10,31 +14,55 @@ function Login() {
   };
 
   const handleLogin = async () => {
-    const urlencoded = new URLSearchParams();
-    urlencoded.append("email", userLogin.email);
-    urlencoded.append("password", userLogin.password);
+    if (
+      !isValidEmail(userLogin.email) &&
+      !isValidPassword(userLogin.password)
+    ) {
+      alert(credentialsError);
+      return;
+    }
+    if (!isValidEmail(userLogin.email)) {
+      alert(emailError);
+      return;
+    }
+    if (!isValidPassword(userLogin.password)) {
+      alert(passwordError);
+      return;
+    }
 
-    const requestOptions = {
-      method: "POST",
-      body: urlencoded,
-    };
+    if (isValidEmail(userLogin.email) && isValidPassword(userLogin.password)) {
+      const urlencoded = new URLSearchParams();
+      urlencoded.append("email", userLogin.email);
+      urlencoded.append("password", userLogin.password);
 
-    try {
-      const response = await fetch(
-        "http://localhost:5000/api/users/login",
-        requestOptions
-      );
-      const result = await response.json();
-      const token = result.token;
+      const requestOptions = {
+        method: "POST",
+        body: urlencoded,
+      };
 
-      if (token) {
-        localStorage.setItem("token", token);
+      try {
+        const response = await fetch(
+          "http://localhost:5000/api/users/login",
+          requestOptions
+        );
+        const result = await response.json();
+        const token = result.token;
+
+        if (token) {
+          localStorage.setItem("token", token);
+        }
+        console.log("Result:", result);
+      } catch (error) {
+        console.log("Login error", error);
       }
-      console.log("Result:", result);
-    } catch (error) {
-      console.log("Login error", error);
     }
   };
+
+  useEffect(() => {
+    setCredentialsError("Wrong credentials");
+    setEmailError("Wrong E-Mail");
+    setPasswordError("Wrong Password");
+  }, [credentialsError, emailError, passwordError]);
 
   return (
     <Box>
