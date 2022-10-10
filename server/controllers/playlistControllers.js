bc38d1c5ae65e3fcb6e8bd0743da70870aaa2e5e
petrikoj <100421329+petrikoj@users.noreply.github.com>
2022-10-10 17:52:56 +0200

    Import cloudinary

--- server/controllers/playlistControllers.js
+++ server/controllers/playlistControllers.js
@@ -1,4 +1,5 @@
 import Playlist from "../models/playlistModel.js";
+import { v2 as cloudinary } from "cloudinary";
 
 // GET all playlists
 
@@ -24,6 +25,48 @@ const getAllPlaylists = async (request, response) => {
   }
 };
 
-// GET playlist by
+// POST new playlist
 
-export { getAllPlaylists };
+const postNewPlaylist = async (request, response) => {
+  const newPlaylist = new Playlist({
+    title: request.body.title,
+    author: request.body.author,
+    description: request.body.description,
+    mood: request.body.mood,
+    image_url: request.body.image_url,
+    songs: request.body.songs,
+    date: request.body.date,
+    likes: request.body.likes,
+  });
+  try {
+    const savePlaylist = await newPlaylist.save();
+    response.status(200).json({ message: "Playlist succesfully created" });
+  } catch (error) {
+    response.status(409).json({ message: "Failed to upload", error: error });
+  }
+};
+
+// Upload playlist img
+
+const uploadPlaylistPicture = async (request, response) => {
+  console.log("Playlist picture request.body:", request.body);
+  console.log("Playlist picture request.file:", request.file);
+
+  try {
+    const uploadResult = await cloudinary.uploader.upload(request.file.path, {
+      folder: "mern-project/playlists",
+    });
+    console.log("uploadResult", uploadResult);
+    response.status(200).json({
+      message: "Upload successful",
+      image_url: uploadResult.url,
+    });
+  } catch (error) {
+    response.status(500).json({
+      message: "Image upload failed",
+      error: error,
+    });
+  }
+};
+
+export { getAllPlaylists, postNewPlaylist, uploadPlaylistPicture };
