9ad9a34afd186590cd863273fd868af8daf21d13
petrikoj <100421329+petrikoj@users.noreply.github.com>
2022-10-10 17:53:35 +0200

    Adding img upload route

--- server/routes/playlistsRoute.js
+++ server/routes/playlistsRoute.js
@@ -1,8 +1,19 @@
 import express from "express";
-import { getAllPlaylists } from "../controllers/playlistControllers.js";
+import { multerUploads } from "../middlewares/multer.js";
+import {
+  getAllPlaylists,
+  postNewPlaylist,
+  uploadPlaylistPicture,
+} from "../controllers/playlistControllers.js";
 
 const router = express.Router();
 
 router.get("/all", getAllPlaylists);
+router.post("/create", postNewPlaylist);
+router.post(
+  "/image-upload",
+  multerUploads.single("image"),
+  uploadPlaylistPicture
+);
 
 export default router;
