53c48cbb668025ad4a93d10ea8a5827899fd18ab
petrikoj <100421329+petrikoj@users.noreply.github.com>
2022-10-11 23:07:18 +0200

    Created AuthContext (mostly working)

--- client/src/context/AuthContext.js
+++ client/src/context/AuthContext.js
@@ -1,7 +1,64 @@
-import { React, createContext, useState, useEffect } from "react";
+import React, { createContext, useState, useEffect } from "react";
+import getToken from "../utils/getToken";
 
-function AuthContext() {
-  return <div>AuthContext</div>;
-}
+export const AuthContext = createContext();
 
-export default AuthContext;
+export const AuthContextProvider = (props) => {
+  const [user, setUser] = useState(null);
+
+  const checkUserStatus = () => {
+    const token = getToken();
+    if (token) {
+      setUser(true);
+      console.log("User logged in");
+    }
+    if (!token) {
+      setUser(false);
+      console.log("User NOT logged in");
+    }
+  };
+
+  const logoutUser = () => {
+    localStorage.removeItem("token");
+    checkUserStatus();
+    console.log("User logged out successfully");
+  };
+
+  const getUser = async (token) => {
+    const myHeaders = new Headers();
+    myHeaders.append("Authorization", "Bearer " + token);
+    const requestOptions = {
+      method: "GET",
+      headers: myHeaders,
+    };
+    try {
+      const response = await fetch(
+        "http://localhost:5000/users/profile",
+        requestOptions
+      );
+      const result = await response.json();
+      setUser({
+        _id: result._id,
+        username: result.username,
+        email: result.email,
+        avatar: result.avatar,
+      });
+      console.log("User from AuthContext:", user);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    checkUserStatus();
+    console.log("useEffect ran");
+  }, [user]);
+
+  return (
+    <AuthContext.Provider
+      value={{ checkUserStatus, logoutUser, getUser, user }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
