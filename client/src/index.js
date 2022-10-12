d4c5d9362ec72f84f7a9b1fd2137f075fba56961
petrikoj <100421329+petrikoj@users.noreply.github.com>
2022-10-12 15:55:25 +0200

    Added browser router and authcontextprovider

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import { AuthContextProvider } from "./context/AuthContext.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <AuthContextProvider>
+        <App />
+      </AuthContextProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
