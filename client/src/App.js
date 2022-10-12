a97f94295e86aa10a34941f4193c7c00c6e74418
petrikoj <100421329+petrikoj@users.noreply.github.com>
2022-10-12 15:54:59 +0200

    Moved provider and browser router

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useContext } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { AuthContextProvider } from "./context/AuthContext.js";
+import { AuthContext, AuthContextProvider } from "./context/AuthContext.js";
 import ProtectedRoute from "./components/userRelated/ProtectedRoute";
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "./styles/theme";
@@ -12,35 +12,33 @@ import PostPlaylist from "./components/userRelated/PostPlaylist";
 import Navbar from "./components/layoutRelated/Navbar.js";
 
 function App() {
+  const { user } = useContext(AuthContext);
+
   return (
-    <BrowserRouter>
-      <ChakraProvider theme={theme}>
-        <AuthContextProvider>
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<LandingView />} />
-            <Route path="/signup" element={<SignUpView />} />
-            <Route path="/login" element={<LoginView />} />
-            <Route
-              path="/profile"
-              element={
-                <ProtectedRoute>
-                  <ProfileView />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/create-playlist"
-              element={
-                <ProtectedRoute>
-                  <PostPlaylist />
-                </ProtectedRoute>
-              }
-            />
-          </Routes>
-        </AuthContextProvider>
-      </ChakraProvider>
-    </BrowserRouter>
+    <ChakraProvider theme={theme}>
+      <Navbar user={user} />
+      <Routes>
+        <Route path="/" element={<LandingView />} />
+        <Route path="/signup" element={<SignUpView />} />
+        <Route path="/login" element={<LoginView />} />
+        <Route
+          path="/profile"
+          element={
+            <ProtectedRoute>
+              <ProfileView />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/create-playlist"
+          element={
+            <ProtectedRoute>
+              <PostPlaylist />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </ChakraProvider>
   );
 }
 
