83181094df4795893f0bcf253dad1784e73c0e14
petrikoj <100421329+petrikoj@users.noreply.github.com>
2022-10-14 17:31:32 +0200

    Replaced upload function

--- server/routes/playlistsRoute.js
+++ server/routes/playlistsRoute.js
@@ -2,7 +2,7 @@ import express from "express";
 import { multerUploads } from "../middlewares/multer.js";
 import {
   getAllPlaylists,
-  postNewPlaylist,
+  uploadNewPlaylist,
   uploadPlaylistPicture,
 } from "../controllers/playlistControllers.js";
 import jwtAuth from "../utils/jwtAuth.js";
@@ -10,7 +10,7 @@ import jwtAuth from "../utils/jwtAuth.js";
 const router = express.Router();
 
 router.get("/all", getAllPlaylists);
-router.post("/create", postNewPlaylist);
+router.post("/create", uploadNewPlaylist);
 router.post(
   "/image-upload",
   multerUploads.single("image"),
