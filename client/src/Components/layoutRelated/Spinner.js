a6b13d69f22c49e85b0d4763039b96707961566f
petrikoj <100421329+petrikoj@users.noreply.github.com>
2022-10-17 12:49:48 +0200

    Created util spinner

---
+++ client/src/Components/layoutRelated/Spinner.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Spinner } from "@chakra-ui/react";
+
+function LoadingSpinner() {
+  return <Spinner size="xl" speed="0.65s" mt="16" />;
+}
+
+export default LoadingSpinner;
