22a16a690e13ded4aed6a4ee4fd998af5ffd8cb4
petrikoj <100421329+petrikoj@users.noreply.github.com>
2022-10-17 19:34:40 +0200

    No changes

--- client/src/Hooks/useFetch.js
+++ client/src/Hooks/useFetch.js
@@ -4,6 +4,7 @@ const useFetch = (url) => {
   const [data, setData] = useState([]);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
+
   const getData = async () => {
     try {
       const response = await fetch(url);
