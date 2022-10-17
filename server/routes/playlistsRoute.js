88440e6703d4bc64272bf9ef026fed6f8e985162
petrikoj <100421329+petrikoj@users.noreply.github.com>
2022-10-17 19:36:27 +0200

    Added route for getPlaylistById

--- server/routes/playlistsRoute.js
+++ server/routes/playlistsRoute.js
@@ -2,6 +2,7 @@ import express from "express";
 import { multerUploads } from "../middlewares/multer.js";
 import {
   getAllPlaylists,
+  getPlaylistById,
   postNewPlaylist,
   uploadPlaylistPicture,
 } from "../controllers/playlistControllers.js";
@@ -10,6 +11,7 @@ import jwtAuth from "../utils/jwtAuth.js";
 const router = express.Router();
 
 router.get("/all", getAllPlaylists);
+router.get("/:id", getPlaylistById);
 router.post("/create", postNewPlaylist);
 router.post(
   "/image-upload",
