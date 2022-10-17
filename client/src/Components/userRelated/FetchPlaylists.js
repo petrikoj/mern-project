a1adfa634a53b920691bde2e03c202989a06b72f
petrikoj <100421329+petrikoj@users.noreply.github.com>
2022-10-17 19:34:27 +0200

    Created two functions for fetching playlists

--- client/src/Components/userRelated/FetchPlaylists.js
+++ client/src/Components/userRelated/FetchPlaylists.js
@@ -1,6 +1,23 @@
 import React from "react";
 import useFetch from "../../hooks/useFetch.js";
-import { Box, Image as Cover, SimpleGrid, Text } from "@chakra-ui/react";
+import {
+  Box,
+  HStack,
+  Image as Cover,
+  SimpleGrid,
+  Table,
+  TableContainer,
+  Tag,
+  Tbody,
+  Td,
+  Tr,
+  Text,
+  Thead,
+  Th,
+  Center,
+  Badge,
+  TagLabel,
+} from "@chakra-ui/react";
 import LoadingSpinner from "../layoutRelated/Spinner.js";
 
 const FetchPlaylists = () => {
@@ -11,29 +28,55 @@ const FetchPlaylists = () => {
   } = useFetch("http://localhost:5000/api/playlists/all");
   console.log(playlists);
   return (
-    <Box>
+    <Box overflowX="hidden" overflowY="auto">
       <SimpleGrid columns={[2, 4]} spacing="10">
         {playlists &&
           playlists.map((list, _id) => {
             return (
               <Box key={_id}>
-                <Text as="b">{list.title}</Text>
-                <Cover
-                  boxSize="36"
-                  src="https://freepngstock.com/assets/img/uploads/owl-ga52a8029e_640.png"
-                />
-                {list.songs.map((song, index) => {
-                  return (
-                    <Box key={index}>
-                      <Text>{song.artist}</Text>
-                      <Text>{song.song_title}</Text>
-                    </Box>
-                  );
-                })}
+                <Center>
+                  <Cover
+                    boxSize="36"
+                    src="https://freepngstock.com/assets/img/uploads/owl-ga52a8029e_640.png"
+                  />
+                </Center>
+                <Center>
+                  <Text as="b">{list.title}</Text>
+                </Center>
+
+                <Tag>
+                  <TagLabel>{list.author}</TagLabel>
+                </Tag>
+                <Badge>{list.songs.length} songs</Badge>
+                <Tag>{list.mood}</Tag>
+                <Text>{list.description}</Text>
+
+                {/* <Box borderRadius="md" bg="red.100">
+                  <TableContainer>
+                    <Table variant="unstyled">
+                      <Thead>
+                        <Tr>
+                          <Th>Artist</Th>
+                          <Th>Title</Th>
+                        </Tr>
+                      </Thead>
+
+                      {list.songs.map((song, index) => {
+                        return (
+                          <Tbody>
+                            <Tr key={index}>
+                              <Td>{song.artist}</Td>
+                              <Td>"{song.song_title}"</Td>
+                            </Tr>
+                          </Tbody>
+                        );
+                      })}
+                    </Table>
+                  </TableContainer>
+                </Box> */}
               </Box>
             );
           })}
-
         {loading && <LoadingSpinner />}
         {error && <p>error</p>}
       </SimpleGrid>
@@ -41,4 +84,17 @@ const FetchPlaylists = () => {
   );
 };
 
-export default FetchPlaylists;
+const useFetchPlaylistById = (id) => {
+  const requestOptions = {
+    method: "GET",
+    redirect: "follow",
+  };
+  const {
+    data: playlist,
+    error,
+    loading,
+  } = useFetch(`http://localhost:5000/api/playlists/${id}`, requestOptions);
+  return { playlist, error, loading };
+};
+
+export { FetchPlaylists, useFetchPlaylistById };
