358555ebd7c81b2974afa5a4dad950d2fcbd0686
petrikoj <100421329+petrikoj@users.noreply.github.com>
2022-10-19 19:11:24 +0200

    Reworked, still under construction

--- client/src/Views/PlaylistView.js
+++ client/src/Views/PlaylistView.js
@@ -1,13 +1,58 @@
 import React from "react";
-import { FetchPlaylists } from "../components/userRelated/FetchPlaylists.js";
-import { Center, Heading, VStack } from "@chakra-ui/react";
+import { useFetchPlaylists } from "../components/userRelated/FetchPlaylists.js";
+import {
+  Badge,
+  Box,
+  Center,
+  Heading,
+  Image,
+  SimpleGrid,
+  Tag,
+  TagLabel,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
+import LoadingSpinner from "../components/layoutRelated/Spinner.js";
+import { Link } from "react-router-dom";
 
 function PlaylistView() {
+  const { playlists, error, loading } = useFetchPlaylists();
   return (
     <Center>
       <VStack>
         <Heading>Playlists</Heading>
-        <FetchPlaylists />
+        <Box>
+          {loading && <LoadingSpinner />}
+          {error && <p>error</p>}
+          <SimpleGrid columns={[2, 4]} spacing="10">
+            {playlists &&
+              playlists.map((list, index) => {
+                return (
+                  <Box key={index} overflowX="hidden" overflowY="auto">
+                    <Link to={`/playlists/${list._id}`}>
+                      <Center>
+                        <Image
+                          boxSize="36"
+                          src={list.img_url}
+                          fit="cover"
+                          borderRadius="md"
+                        />
+                      </Center>
+                      <Center>
+                        <VStack>
+                          <Text as="b">{list.title}</Text>
+                          <Text>{list.creator.username}</Text>
+                        </VStack>
+                      </Center>
+                      <Badge>{list.songs.length} songs</Badge>
+                      <Badge>{list.mood}</Badge>
+                      <Text>{list.description}</Text>
+                    </Link>
+                  </Box>
+                );
+              })}
+          </SimpleGrid>
+        </Box>
       </VStack>
     </Center>
   );
