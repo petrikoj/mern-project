5003542a26c068d359594fb22066930009ce4823
petrikoj <100421329+petrikoj@users.noreply.github.com>
2022-10-19 19:12:03 +0200

    No real changes

--- server/controllers/playlistControllers.js
+++ server/controllers/playlistControllers.js
@@ -7,14 +7,7 @@ import User from "../models/userModel.js";
 
 const getAllPlaylists = async (request, response) => {
   const allPlaylists = await Playlist.find({})
-    /* .populate({
-      path: "author",
-      select: "username",
-    })
-    .populate({
-      path: "author_id",
-      select: "_id",
-    }) */
+    .populate({ path: "creator", select: ["username", "avatar"] })
     .exec();
   console.log("Logging all playlists:", allPlaylists);
   try {
@@ -31,53 +24,72 @@ const getAllPlaylists = async (request, response) => {
   }
 };
 
-// GET playlist by ID
+// GET playlist by _id
 
 const getPlaylistById = async (request, response) => {
-  let wantedPlaylistId = request.params.id;
-  await Playlist.findById(wantedPlaylistId)
-    .populate("title")
-    .exec(function (error, playlist) {
-      try {
-        if (playlist === undefined || playlist === null) {
-          response.status(200).json({ msg: "Object not found" });
-          console.log("Object not found");
-        } else {
-          response.status(200).json(playlist);
-          console.log("Server/getPlaylistbyId:", playlist);
-        }
-      } catch (eror) {
-        response.status(500).json({
-          message: "Server failed",
-          error: error,
-        });
-      }
+  const wantedPlaylistId = request.params._id;
+  const playlist = await Playlist.findOne({ _id: wantedPlaylistId }).exec();
+  try {
+    if (playlist === undefined || playlist === null) {
+      response.status(200).json({ msg: "Object not found" });
+      console.log("Object not found");
+    } else {
+      response.status(200).json(playlist);
+      console.log("Server/getPlaylistbyId:", playlist);
+    }
+  } catch (error) {
+    response.status(500).json({
+      message: "Server failed",
+      error: error,
     });
+  }
 };
+
+// GET Playlists by user._id
+
+/* const getPlaylistsByUserId = async (request, response) => {
+  const userId = request.params._id;
+  const playlistsFromUserId = await Playlist.find({
+    creator: userId,
+  }).exec();
+  try {
+    if (playlistsFromUserId === undefined || playlistsFromUserId === null) {
+      response.status(200).json({ msg: "Object not found" });
+      console.log("Object not found");
+    } else {
+      response.status(200).json(playlistsFromUserId);
+      console.log("Server/getPlaylistsbyUserId:", playlistsFromUserId);
+    }
+  } catch (error) {
+    response.status(500).json({
+      message: "Server failed",
+      error: error,
+    });
+  }
+}; */
 
 // POST new playlist
 
 const postNewPlaylist = async (request, response) => {
   const newPlaylist = new Playlist({
     title: request.body.title,
-    author: request.body.author,
-    author_id: request.body.author_id,
+    creator: request.body.creator,
     description: request.body.description,
     mood: request.body.mood,
-    image_url: request.body.image_url,
+    img_url: request.body.img_url,
     songs: request.body.songs,
     date: request.body.date,
     likes: request.body.likes,
   });
   try {
-    const savePlaylist = await newPlaylist.save();
+    const savedPlaylist = await newPlaylist.save();
     response.status(200).json({ message: "Playlist succesfully created" });
   } catch (error) {
     response.status(409).json({ message: "Failed to upload", error: error });
   }
   try {
     await User.findOneAndUpdate(
-      { _id: request.body.author_id },
+      { _id: request.body.creator },
       { $push: { playlists: newPlaylist._id } },
       { new: true }
     );
