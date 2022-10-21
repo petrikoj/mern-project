0337a7202c3e2e105eda4a516b2c13327ef537d5
petrikoj <100421329+petrikoj@users.noreply.github.com>
2022-10-21 18:50:09 +0200

    Troubleshooting

--- client/src/Views/ProfileView.js
+++ client/src/Views/ProfileView.js
@@ -23,105 +23,115 @@ import { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
 import { Link, useParams } from "react-router-dom";
 import { useFetchUser } from "../components/userRelated/FetchPlaylists";
+import LoadingSpinner from "../components/layoutRelated/Spinner";
 
 function ProfileView() {
-  const { userProfile, setUserProfile, getUserProfile } =
-    useContext(AuthContext);
-  console.log(userProfile.playlists);
+  const { userProfile } = useContext(AuthContext);
   const { _id } = useParams();
   const { myUser, error, loading } = useFetchUser(_id);
   console.log("Result of useFetchUser:", myUser);
 
   return (
-    <Center>
-      <VStack>
-        <Divider />
-        <Heading>{myUser.username}'s Profile</Heading>
-        <Tabs
-          isFitted
-          size={["md", "lg"]}
-          variant="soft-rounded"
-          colorScheme="gray"
-        >
-          <TabList m="3">
-            <Tab>
-              <VStack>
-                <Icon as={TbPlaylist} />
-                <Text>Playlists</Text>
-              </VStack>
-            </Tab>
-            <Tab>
-              <VStack>
-                <Icon as={HiHeart} />
-                <Text>Favorites</Text>
-              </VStack>
-            </Tab>
-            <Tab>
-              <VStack>
-                <Icon as={IoMdSettings} />
-                <Text>Settings</Text>
-              </VStack>
-            </Tab>
-          </TabList>
-          <Divider />
-          <TabPanels>
-            <TabPanel>
-              <>
-                {myUser.playlists?.map((list, index) => {
-                  return (
-                    <Box
-                      key={index}
-                      border="1px"
-                      borderRadius="md"
-                      p="1.5"
-                      m="1"
-                    >
-                      <Link to={`/playlists/${list._id}`}>
-                        <HStack>
-                          <Image
-                            src={list.img_url}
-                            borderRadius="md"
-                            boxSize="16"
-                          />
-                          <VStack align="start">
-                            <Text as="b">{list.title}</Text>
-                            <Badge>{list.songs.length} songs</Badge>
-                          </VStack>
-                        </HStack>
-                      </Link>
-                    </Box>
-                  );
-                })}
-              </>
-            </TabPanel>
-            <TabPanel>
-              {myUser.liked?.map((list, index) => {
-                return (
-                  <Box key={index} border="1px" borderRadius="md" p="1.5" m="1">
-                    <Link to={`/playlists/${list._id}`}>
-                      <HStack>
-                        <Image
-                          src={list.img_url}
+    <>
+      {loading && <LoadingSpinner />}
+      {myUser && (
+        <Center>
+          <VStack>
+            <Divider />
+            <Heading>{myUser.username}'s Profile</Heading>
+            <Tabs
+              isFitted
+              size={["md", "lg"]}
+              variant="soft-rounded"
+              colorScheme="gray"
+            >
+              <TabList m="3">
+                <Tab>
+                  <VStack>
+                    <Icon as={TbPlaylist} />
+                    <Text>Playlists</Text>
+                  </VStack>
+                </Tab>
+                <Tab>
+                  <VStack>
+                    <Icon as={HiHeart} />
+                    <Text>Favorites</Text>
+                  </VStack>
+                </Tab>
+                <Tab>
+                  <VStack>
+                    <Icon as={IoMdSettings} />
+                    <Text>Settings</Text>
+                  </VStack>
+                </Tab>
+              </TabList>
+              <Divider />
+              <TabPanels>
+                <TabPanel>
+                  <>
+                    {myUser.playlists?.map((list, index) => {
+                      return (
+                        <Box
+                          key={index}
+                          border="1px"
                           borderRadius="md"
-                          boxSize="16"
-                        />
-                        <VStack align="start">
-                          <Text as="b">{list.title}</Text>
-                          <Badge>{list.songs.length} songs</Badge>
-                        </VStack>
-                      </HStack>
-                    </Link>
-                  </Box>
-                );
-              })}
-            </TabPanel>
-            <TabPanel>
-              <p>Settings ...</p>
-            </TabPanel>
-          </TabPanels>
-        </Tabs>
-      </VStack>
-    </Center>
+                          p="1.5"
+                          m="1"
+                        >
+                          <Link to={`/playlists/${list._id}`}>
+                            <HStack>
+                              <Image
+                                src={list.img_url}
+                                borderRadius="md"
+                                boxSize="16"
+                              />
+                              <VStack align="start">
+                                <Text as="b">{list.title}</Text>
+                                <Badge>{list.songs.length} songs</Badge>
+                              </VStack>
+                            </HStack>
+                          </Link>
+                        </Box>
+                      );
+                    })}
+                  </>
+                </TabPanel>
+                <TabPanel>
+                  {myUser.liked?.map((list, index) => {
+                    return (
+                      <Box
+                        key={index}
+                        border="1px"
+                        borderRadius="md"
+                        p="1.5"
+                        m="1"
+                      >
+                        <Link to={`/playlists/${list._id}`}>
+                          <HStack>
+                            <Image
+                              src={list.img_url}
+                              borderRadius="md"
+                              boxSize="16"
+                            />
+                            <VStack align="start">
+                              <Text as="b">{list.title}</Text>
+                              <Badge>{list.songs.length} songs</Badge>
+                            </VStack>
+                          </HStack>
+                        </Link>
+                      </Box>
+                    );
+                  })}
+                </TabPanel>
+                <TabPanel>
+                  <p>Settings ...</p>
+                </TabPanel>
+              </TabPanels>
+            </Tabs>
+          </VStack>
+        </Center>
+      )}
+    </>
   );
 }
 
