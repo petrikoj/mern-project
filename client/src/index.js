d33b6185e126f2422193d961568f74e377136ef3
petrikoj <100421329+petrikoj@users.noreply.github.com>
2022-10-21 18:47:40 +0200

    Imported playlistController

--- client/src/index.js
+++ client/src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { AuthContextProvider } from "./context/AuthContext.js";
+import { PlaylistContextProvider } from "./context/PlaylistContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthContextProvider>
-        <App />
+        <PlaylistContextProvider>
+          <App />
+        </PlaylistContextProvider>
       </AuthContextProvider>
     </BrowserRouter>
   </React.StrictMode>
