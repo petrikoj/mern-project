83b34c8976dd7585cccb7ec4b7196f6c528d6f15
petrikoj <100421329+petrikoj@users.noreply.github.com>
2022-10-22 12:39:56 +0200

    Created 404 View

---
+++ client/src/Views/NoMatchView.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Center, Icon, IconButton, Text, VStack } from "@chakra-ui/react";
+import { TbError404 } from "react-icons/tb";
+import { ArrowLeftIcon, ArrowRightIcon } from "@chakra-ui/icons";
+
+function NoMatchView() {
+  const navigate = useNavigate();
+  return (
+    <Center>
+      <VStack>
+        <Icon as={TbError404} boxSize="44" color="blackAlpha.900" />
+        {/* <Heading>404</Heading> */}
+        <Text as="b">This page doesn't exist.</Text>
+        <IconButton
+          leftIcon={<ArrowLeftIcon />}
+          onClick={() => navigate(-1)}
+          size="lg"
+          p="2"
+        >
+          <Text>Back</Text>
+        </IconButton>
+        <Text>or</Text>
+        <Link to={"/"}>
+          <IconButton rightIcon={<ArrowRightIcon />} size="lg" p="2">
+            <Text>Home</Text>
+          </IconButton>
+        </Link>
+      </VStack>
+    </Center>
+  );
+}
+
+export default NoMatchView;
