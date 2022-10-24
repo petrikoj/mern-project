0842bd2331b1fe9c0c02e6f8f008112d2710a969
petrikoj <100421329+petrikoj@users.noreply.github.com>
2022-10-24 23:37:21 +0200

    Removed like func

--- client/src/context/PlaylistContext.js
+++ client/src/context/PlaylistContext.js
@@ -7,7 +7,6 @@ export const PlaylistContext = createContext();
 
 export const PlaylistContextProvider = (props) => {
   const { userProfile } = useContext(AuthContext);
-  const [isLiked, setIsLiked] = useState(false);
 
   const [myPlaylists, setMyPlaylists] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -23,6 +22,7 @@ export const PlaylistContextProvider = (props) => {
       const result = await response.json();
       setMyPlaylists(result);
       setLoading(false);
+      console.log("Fetch result from PlaylistContext", result);
     } catch (error) {
       setLoading(false);
       setError(error);
@@ -50,90 +50,6 @@ export const PlaylistContextProvider = (props) => {
     }
 
     return { myPlaylists, error, loading };
-  };
-
-  // Like + Unlike a Playlist (PUSH and PULL)
-
-  // PUSH like to db
-
-  const likePlaylist = async () => {
-    const myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
-
-    const urlencoded = new URLSearchParams();
-    urlencoded.append("user_id", userProfile._id);
-    urlencoded.append("playlist_id", myPlaylists._id);
-
-    const requestOptions = {
-      method: "PUT",
-      headers: myHeaders,
-      body: urlencoded,
-      redirect: "follow",
-    };
-    try {
-      const response = await fetch(
-        "http://localhost:5000/api/users/like",
-        requestOptions
-      );
-      const result = await response.json();
-      console.log(result);
-      setIsLiked(true);
-      toast({
-        title: `${result.message}`,
-        status: "success",
-        duration: 1500,
-        isClosable: true,
-      });
-    } catch (error) {
-      toast({
-        description: `${error.message}`,
-        status: "error",
-        duration: 1500,
-        isClosable: true,
-      });
-      console.log(error);
-    }
-  };
-
-  // PULL like from db
-
-  const unlikePlaylist = async () => {
-    const myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
-
-    const urlencoded = new URLSearchParams();
-    urlencoded.append("user_id", userProfile._id);
-    urlencoded.append("playlist_id", myPlaylists._id);
-
-    const requestOptions = {
-      method: "PUT",
-      headers: myHeaders,
-      body: urlencoded,
-      redirect: "follow",
-    };
-    try {
-      const response = await fetch(
-        "http://localhost:5000/api/users/unlike",
-        requestOptions
-      );
-      const result = await response.json();
-      console.log(result);
-      setIsLiked(false);
-      toast({
-        title: `${result.message}`,
-        status: "success",
-        duration: 1500,
-        isClosable: true,
-      });
-    } catch (error) {
-      toast({
-        description: `${error.message}`,
-        status: "error",
-        duration: 1500,
-        isClosable: true,
-      });
-      console.log(error);
-    }
   };
 
   // useEffect for keeping the state of my playlists
