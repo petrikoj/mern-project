a4efb1c282620276993eca1e0bdffbf940c33cf3
petrikoj <100421329+petrikoj@users.noreply.github.com>
2022-10-24 23:37:55 +0200

    Implemented new like btn (still not fully working)

--- client/src/Views/DetailView.js
+++ client/src/Views/DetailView.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import { useFetchPlaylistById } from "../components/userRelated/FetchPlaylists.js";
 import {
   Box,
@@ -16,38 +16,69 @@ import {
   Text,
   Tag,
   Divider,
+  Stat,
+  StatLabel,
+  StatNumber,
+  Icon,
+  HStack,
+  Flex,
 } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
 import LoadingSpinner from "../components/layoutRelated/Spinner.js";
 import { ErrorAlert } from "../components/layoutRelated/Alerts.js";
+import { AuthContext } from "../context/AuthContext.js";
+import UnlikeButton from "../components/userRelated/UnlikeButton.js";
+import LikeButton from "../components/userRelated/LikeButton.js";
+import { PlaylistContext } from "../context/PlaylistContext.js";
 /* import { useContext } from "react";
 import { PlaylistContext } from "../context/PlaylistContext.js"; */
 
 function DetailView() {
   const { _id } = useParams();
   const { playlist, error, loading } = useFetchPlaylistById(_id);
+  const { userProfile, user } = useContext(AuthContext);
+
+  const [likeCount, setLikeCount] = useState(null);
+
   // Not Working with PlaylistContext =/
   return (
-    <Center>
+    <Center overflowY="scroll" m="2" w="auto" h="auto">
       <VStack>
         {loading && <LoadingSpinner />}
         {error && <ErrorAlert message={error.message} />}
         {playlist && (
           <>
             <Heading>{playlist.title}</Heading>
-            <Divider />
             <Image
               src={playlist.img_url}
               alt="playlist picture"
-              boxSize={["max-content", "lg"]}
+              boxSize={["fit-content", "lg"]}
               borderRadius="lg"
             />
-            <Divider />
+            <Divider borderColor="blackAlpha.900" />
             <Tag>{playlist.mood}</Tag>
 
             <Text>{playlist.description}</Text>
+            <Flex align="center">
+              <Box mr="-1.5" mb="1.5">
+                <Text fontSize="lg" fontWeight="semibold">
+                  {playlist.liked_by?.length}
+                </Text>
+              </Box>
+              {playlist.liked_by?.includes(userProfile._id) ? (
+                <UnlikeButton
+                  playlist_id={playlist._id}
+                  user_id={userProfile._id}
+                />
+              ) : (
+                <LikeButton
+                  playlist_id={playlist._id}
+                  user_id={userProfile._id}
+                />
+              )}
+            </Flex>
 
-            <Box borderRadius="md" bg="red.100">
+            <Box borderRadius="md" bg="red.100" w="xs" overflowY="scroll">
               <TableContainer>
                 <Table variant="unstyled">
                   <Thead>
