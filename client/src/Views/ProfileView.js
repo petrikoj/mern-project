7eb18f4919c6585853103541594a82060254b5c8
petrikoj <100421329+petrikoj@users.noreply.github.com>
2022-10-24 23:36:18 +0200

    Cosmetics

--- client/src/Views/ProfileView.js
+++ client/src/Views/ProfileView.js
@@ -6,6 +6,7 @@ import {
   Heading,
   HStack,
   Icon,
+  IconButton,
   Image,
   Tab,
   TabList,
@@ -14,19 +15,22 @@ import {
   Tabs,
   Tag,
   Text,
+  useToast,
   VStack,
 } from "@chakra-ui/react";
+import { EditIcon } from "@chakra-ui/icons";
 import { TbPlaylist } from "react-icons/tb";
 import { HiHeart } from "react-icons/hi";
 import { IoMdSettings } from "react-icons/io";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../context/AuthContext";
 import { Link, useParams } from "react-router-dom";
 import { useFetchUser } from "../components/userRelated/FetchPlaylists";
 import LoadingSpinner from "../components/layoutRelated/Spinner";
+//import LikeAndUnlikeButton from "../components/userRelated/LikeAndUnlikeButton";
+import UnlikeButton from "../components/userRelated/UnlikeButton";
 
 function ProfileView() {
-  const { userProfile } = useContext(AuthContext);
   const { _id } = useParams();
   const { myUser, error, loading } = useFetchUser(_id);
   console.log("Result of useFetchUser:", myUser);
@@ -35,12 +39,14 @@ function ProfileView() {
     <>
       {loading && <LoadingSpinner />}
       {myUser && (
-        <Center>
+        <Center overflowX="hidden">
           <VStack>
-            <Divider />
+            <Divider borderColor="blackAlpha.900" />
             <Heading>{myUser.username}'s Profile</Heading>
+
             <Tabs
               isFitted
+              isLazy
               size={["md", "lg"]}
               variant="soft-rounded"
               colorScheme="gray"
@@ -65,18 +71,54 @@ function ProfileView() {
                   </VStack>
                 </Tab>
               </TabList>
-              <Divider />
+              <Divider borderColor="blackAlpha.900" />
               <TabPanels>
-                <TabPanel>
+                <TabPanel overflowY="auto">
                   <>
                     {myUser.playlists?.map((list, index) => {
                       return (
+                        <HStack>
+                          <Box
+                            key={index}
+                            border="1px"
+                            borderRadius="md"
+                            p="1.5"
+                            m="1"
+                            w={["64", "xl", "2xl"]}
+                            h="auto"
+                          >
+                            <Link to={`/playlists/${list._id}`}>
+                              <HStack>
+                                <Image
+                                  src={list.img_url}
+                                  borderRadius="md"
+                                  boxSize="16"
+                                />
+                                <VStack align="start">
+                                  <Text as="b">{list.title}</Text>
+                                  <Badge>{list.songs.length} songs</Badge>
+                                </VStack>
+                              </HStack>
+                            </Link>
+                          </Box>
+                          <IconButton icon={<EditIcon />} variant="ghost" />
+                        </HStack>
+                      );
+                    })}
+                  </>
+                </TabPanel>
+                <TabPanel overflowY="auto">
+                  {myUser.liked?.map((list, index) => {
+                    return (
+                      <HStack>
                         <Box
                           key={index}
                           border="1px"
                           borderRadius="md"
                           p="1.5"
                           m="1"
+                          w={["64", "xl", "2xl"]}
+                          h="auto"
                         >
                           <Link to={`/playlists/${list._id}`}>
                             <HStack>
@@ -92,34 +134,11 @@ function ProfileView() {
                             </HStack>
                           </Link>
                         </Box>
-                      );
-                    })}
-                  </>
-                </TabPanel>
-                <TabPanel>
-                  {myUser.liked?.map((list, index) => {
-                    return (
-                      <Box
-                        key={index}
-                        border="1px"
-                        borderRadius="md"
-                        p="1.5"
-                        m="1"
-                      >
-                        <Link to={`/playlists/${list._id}`}>
-                          <HStack>
-                            <Image
-                              src={list.img_url}
-                              borderRadius="md"
-                              boxSize="16"
-                            />
-                            <VStack align="start">
-                              <Text as="b">{list.title}</Text>
-                              <Badge>{list.songs.length} songs</Badge>
-                            </VStack>
-                          </HStack>
-                        </Link>
-                      </Box>
+                        <UnlikeButton
+                          playlist_id={list._id}
+                          user_id={myUser._id}
+                        />
+                      </HStack>
                     );
                   })}
                 </TabPanel>
