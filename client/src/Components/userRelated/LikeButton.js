185d0ec001849f1864882b4326d31e2591185ccf
petrikoj <100421329+petrikoj@users.noreply.github.com>
2022-10-24 23:35:13 +0200

    Still need to update the state

---
+++ client/src/Components/userRelated/LikeButton.js
@@ -0,0 +1,57 @@
+import { Button, Icon, useToast } from "@chakra-ui/react";
+import { useContext, useEffect } from "react";
+import { BsBookmarkHeart } from "react-icons/bs";
+import { AuthContext } from "../../context/AuthContext";
+import { PlaylistContext } from "../../context/PlaylistContext";
+
+const LikeButton = ({ user_id, playlist_id }) => {
+  const toast = useToast();
+
+  const likePlaylist = async () => {
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+
+    const urlencoded = new URLSearchParams();
+    urlencoded.append("user_id", user_id);
+    urlencoded.append("playlist_id", playlist_id);
+
+    const requestOptions = {
+      method: "PUT",
+      headers: myHeaders,
+      body: urlencoded,
+      redirect: "follow",
+    };
+    try {
+      const response = await fetch(
+        "http://localhost:5000/api/users/like",
+        requestOptions
+      );
+      const result = await response.json();
+      console.log(result);
+      toast({
+        title: `${result.message}`,
+        status: "success",
+        duration: 1500,
+        isClosable: true,
+        position: "top",
+      });
+    } catch (error) {
+      toast({
+        description: `${error.message}`,
+        status: "error",
+        duration: 1500,
+        isClosable: true,
+        position: "top",
+      });
+      console.log(error);
+    }
+  };
+
+  return (
+    <Button variant="unstyled" size="lg" onClick={likePlaylist}>
+      <Icon as={BsBookmarkHeart} />
+    </Button>
+  );
+};
+
+export default LikeButton;
