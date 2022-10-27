f6618c8586ab0d0d21c2856376accfc6bcdaf4e4
petrikoj <100421329+petrikoj@users.noreply.github.com>
2022-10-27 17:36:39 +0200

    Reworked fetchPlaylistById

--- client/src/Components/userRelated/FetchPlaylists.js
+++ client/src/Components/userRelated/FetchPlaylists.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import useFetch from "../../hooks/useFetch.js";
 
 const useFetchPlaylists = () => {
@@ -10,18 +11,38 @@ const useFetchPlaylists = () => {
   return { playlists, error, loading };
 };
 
+// GET playlist by ID
+
 const useFetchPlaylistById = (_id) => {
-  const requestOptions = {
-    method: "GET",
-    redirect: "follow",
+  const [playlist, setPlaylist] = useState({});
+  const [comments, setComments] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const getPlaylist = async () => {
+    try {
+      const response = await fetch(
+        `http://localhost:5000/api/playlists/${_id}`
+      );
+      const result = await response.json();
+      setPlaylist(result);
+      setComments(result.comments);
+      setLoading(false);
+    } catch (error) {
+      setError(error);
+      console.log(error);
+      setLoading(false);
+    }
   };
-  const {
-    data: playlist,
-    error,
-    loading,
-  } = useFetch(`http://localhost:5000/api/playlists/${_id}`, requestOptions);
-  return { playlist, error, loading };
+
+  useEffect(() => {
+    getPlaylist();
+  }, [_id]);
+
+  return { playlist, comments, setComments, error, loading, getPlaylist };
 };
+
+// GET user by ID
 
 const useFetchUser = (_id) => {
   const requestOptions = {
