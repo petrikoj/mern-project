3fdd2e7ef3093afffed3241c425f0093c334c34d
petrikoj <100421329+petrikoj@users.noreply.github.com>
2022-10-30 20:19:26 +0100

    No changes here

--- server/routes/usersRoute.js
+++ server/routes/usersRoute.js
@@ -18,13 +18,13 @@ const router = express.Router();
 router.get("/all", getAllUsers);
 router.get("/profile", jwtAuth, getUserProfile);
 router.get("/profile/:_id", getUserById);
-
 router.post("/image-upload", multerUploads.single("image"), uploadUserPicture);
 router.post("/signup", signUp);
 router.post("/login", login);
-
 router.put("/likehandling", likeOrUnlikePlaylist);
 router.put("/like", likePlaylist);
 router.put("/unlike", removeLikePlaylist);
+
+//router.put("/profile/:id/update-profile", updateUser);
 
 export default router;
