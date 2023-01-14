c67cf3caccc549b24addf83708144ac33b3e67a7
petrikoj <100421329+petrikoj@users.noreply.github.com>
2023-01-14 02:33:12 +0100

    Added guest login feature

--- client/src/Views/SignUpView.js
+++ client/src/Views/SignUpView.js
@@ -1,11 +1,21 @@
-import { Center, Heading, VStack } from "@chakra-ui/react";
+import { ArrowForwardIcon } from "@chakra-ui/icons";
+import { Center, Heading, IconButton, Text, VStack } from "@chakra-ui/react";
 import SignUp from "../components/userRelated/SignUp";
+import useGuestLogin from "../hooks/useGuestLogin";
 
 function SignUpView() {
+  const { handleLogin } = useGuestLogin();
   return (
     <Center>
       <VStack>
-        <Heading>Sign Up</Heading>
+        <Heading>Sign in as guest</Heading>
+        <IconButton
+          icon={<ArrowForwardIcon boxSize="6" />}
+          w="40"
+          onClick={handleLogin}
+        />
+        <Text>or</Text>
+        <Heading>Create an account</Heading>
         <SignUp />
       </VStack>
     </Center>
