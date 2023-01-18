63526f33ed8ffccf588b88a30e72bb156434944d
petrikoj <100421329+petrikoj@users.noreply.github.com>
2023-01-18 23:34:42 +0100

    Update user profile for email and username working

--- client/src/Components/userRelated/UpdateProfile.js
+++ client/src/Components/userRelated/UpdateProfile.js
@@ -1,8 +1,11 @@
 import { useState, useContext, useRef } from "react";
 import { AuthContext } from "../../context/AuthContext";
+import { useParams } from "react-router-dom";
+import useGetMeToken from "../../hooks/useGetMeToken";
+import { baseURL } from "../../utils/getServerUrl";
 import { CheckIcon, CloseIcon, EditIcon } from "@chakra-ui/icons";
 import {
-  Avatar,
+  Button,
   ButtonGroup,
   Container,
   Divider,
@@ -19,12 +22,62 @@ import {
 
 function UpdateProfile() {
   const { userProfile, setUserProfile } = useContext(AuthContext);
+  const { _id } = useParams();
+  const { getMyToken } = useGetMeToken();
   const email = useRef();
   const username = useRef();
-  /* Here's a custom control */
+  const [newEmail, setNewEmail] = useState("");
+  const [newUsername, setNewUsername] = useState("");
+
+  const handleSubmit = () => {
+    setNewEmail(email.current.value);
+    setNewUsername(username.current.value);
+  };
+
+  const updateUser = async (event) => {
+    event.preventDefault();
+    const token = getMyToken();
+    if (!token) {
+      alert("No token");
+    }
+    if (token) {
+      try {
+        const myHeaders = new Headers();
+        myHeaders.append("Authorization", `Bearer ${token}`);
+        myHeaders.append("Content-Type", "application/json");
+        const toBeUpdated = JSON.stringify({
+          ...userProfile,
+          email: newEmail,
+          username: newUsername,
+        });
+        const requestOptions = {
+          method: "PATCH",
+          headers: myHeaders,
+          body: toBeUpdated,
+        };
+        const response = await fetch(
+          baseURL + `/api/users/profile/${_id}/update-profile`,
+          requestOptions
+        );
+        const result = await response.json();
+        setUserProfile(result.userUpdated);
+        console.log(result);
+      } catch (error) {
+        console.log(error);
+        return alert(error.message);
+      }
+    } else {
+      return alert("No token for this user");
+    }
+  };
+
   function EditableControls() {
-    const { isEditing, getSubmitButtonProps, getCancelButtonProps } =
-      useEditableControls();
+    const {
+      isEditing,
+      getSubmitButtonProps,
+      getCancelButtonProps,
+      getEditButtonProps,
+    } = useEditableControls();
 
     return isEditing ? (
       <ButtonGroup justifyContent="center" size="sm" spacing="4" pt="2">
@@ -39,24 +92,29 @@ function UpdateProfile() {
           bgColor="red.300"
         />
       </ButtonGroup>
-    ) : null;
+    ) : (
+      <Flex justifyContent="center">
+        <IconButton size="sm" icon={<EditIcon />} {...getEditButtonProps()} />
+      </Flex>
+    );
   }
 
   return (
     <VStack spacing="6">
       <Container>
         <Text fontWeight="extrabold">Email</Text>
+
         <Editable
           defaultValue={userProfile.email}
-          ref={email}
+          onSubmit={handleSubmit}
           fontSize="lg"
           isPreviewFocusable={true}
           selectAllOnFocus={false}
         >
           <EditablePreview />
-          {/* Here is the custom input */}
           <Input
             as={EditableInput}
+            ref={email}
             bgColor="whiteAlpha.900"
             focusBorderColor="blackAlpha.900"
           />
@@ -68,15 +126,15 @@ function UpdateProfile() {
         <Text fontWeight="extrabold">Username</Text>
         <Editable
           defaultValue={userProfile.username}
-          ref={username}
+          onSubmit={handleSubmit}
           fontSize="lg"
           isPreviewFocusable={true}
           selectAllOnFocus={false}
         >
           <EditablePreview />
-          {/* Here is the custom input */}
           <Input
             as={EditableInput}
+            ref={username}
             bgColor="whiteAlpha.900"
             focusBorderColor="blackAlpha.900"
           />
@@ -84,6 +142,7 @@ function UpdateProfile() {
         </Editable>
       </Container>
       <Divider borderColor="blackAlpha.900" />
+      <Button onClick={updateUser}>Save changes</Button>
     </VStack>
   );
 }
