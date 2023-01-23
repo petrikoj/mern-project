f1a0c5fabb3284ac4f16d0b62205a05c0ac28583
petrikoj <100421329+petrikoj@users.noreply.github.com>
2023-01-23 14:47:20 +0100

    Added username validator

--- client/src/utils/validators.js
+++ client/src/utils/validators.js
@@ -6,4 +6,8 @@ function isValidPassword(password) {
   return /^[a-zA-Z0-9]{6,30}$/.test(password);
 }
 
-export { isValidEmail, isValidPassword };
+function isValidUsername(username) {
+  return /^[A-Za-z0-9]+(?:[ _-][A-Za-z0-9]+)*$/.test(username);
+}
+
+export { isValidEmail, isValidPassword, isValidUsername };
