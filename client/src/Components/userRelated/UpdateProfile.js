ea13103e7779c9dceee7349cee656210def660cb
petrikoj <100421329+petrikoj@users.noreply.github.com>
2023-01-23 14:47:07 +0100

    Imported custom update func

--- client/src/Components/userRelated/UpdateProfile.js
+++ client/src/Components/userRelated/UpdateProfile.js
@@ -1,5 +1,6 @@
 import { useState, useContext, useRef } from "react";
 import { AuthContext } from "../../context/AuthContext";
+import { PlaylistContext } from "../../context/PlaylistContext";
 import { useParams } from "react-router-dom";
 import useGetMeToken from "../../hooks/useGetMeToken";
 import { baseURL } from "../../utils/getServerUrl";
@@ -10,42 +11,66 @@ import {
   ButtonGroup,
   Container,
   Divider,
-  Editable,
-  EditableInput,
-  EditablePreview,
   Flex,
   IconButton,
   Input,
   Text,
-  useEditableControls,
+  FormControl,
   useToast,
   VStack,
+  FormHelperText,
 } from "@chakra-ui/react";
-import { isValidEmail } from "../../utils/validators";
+import { isValidEmail, isValidUsername } from "../../utils/validators";
 
 function UpdateProfile() {
   const { userProfile, setUserProfile } = useContext(AuthContext);
+  const { myPlaylists, setMyPlaylists } = useContext(PlaylistContext);
   const { _id } = useParams();
   const { getMyToken } = useGetMeToken();
   const email = useRef();
   const username = useRef();
   const [newEmail, setNewEmail] = useState("");
   const [newUsername, setNewUsername] = useState("");
+  const [isEditingEmail, setIsEditingEmail] = useState(false);
+  const [isEditingUsername, setIsEditingUsername] = useState(false);
+  const [emailError, setEmailError] = useState(false);
+  const [usernameError, setUsernameError] = useState(false);
   const toast = useToast();
 
+  // Handling editable inputs
+
+  const openEmailEditable = () => setIsEditingEmail(true);
+  const closeEmailEditable = () => {
+    setIsEditingEmail(false);
+    setEmailError(false);
+  };
+  const openUsernameEditable = () => setIsEditingUsername(true);
+  const closeUsernameEditable = () => {
+    setIsEditingUsername(false);
+    setUsernameError(false);
+  };
   const handleEmailSubmit = () => {
+    if (!isValidEmail(email.current.value)) {
+      setEmailError(true);
+    }
     if (isValidEmail(email.current.value)) {
+      setIsEditingEmail(false);
+      setEmailError(false);
       setNewEmail(email.current.value);
-    } else {
-      alert("Invalid Email");
     }
   };
-
   const handleUsernameSubmit = () => {
-    if (username.current.value !== "") {
+    if (!isValidUsername(username.current.value)) {
+      setUsernameError(true);
+    }
+    if (isValidUsername(username.current.value)) {
+      setIsEditingUsername(false);
+      setUsernameError(false);
       setNewUsername(username.current.value);
     }
   };
+
+  // PATCH req to update user
 
   const updateUser = async (event) => {
     event.preventDefault();
@@ -74,6 +99,15 @@ function UpdateProfile() {
         );
         const result = await response.json();
         setUserProfile(result.userUpdated);
+        //TODO - updating state of playlists after user update not working yet
+        setMyPlaylists(
+          myPlaylists.map((list) => {
+            if (list.creator._id === result.userUpdated._id) {
+              return { ...list, username: result.userUpdated.username };
+            }
+            return list;
+          })
+        );
         toast({
           duration: 2000,
           position: "top",
@@ -102,78 +136,97 @@ function UpdateProfile() {
     }
   };
 
-  function EditableControls() {
-    const {
-      isEditing,
-      getSubmitButtonProps,
-      getCancelButtonProps,
-      getEditButtonProps,
-    } = useEditableControls();
-
-    return isEditing ? (
-      <ButtonGroup justifyContent="center" size="sm" spacing="4" pt="2">
-        <IconButton
-          icon={<CheckIcon />}
-          {...getSubmitButtonProps()}
-          bgColor="green.200"
-        />
-        <IconButton
-          icon={<CloseIcon />}
-          {...getCancelButtonProps()}
-          bgColor="red.300"
-          _hover={{ bg: "pink.500" }}
-        />
-      </ButtonGroup>
-    ) : (
-      <Flex justifyContent="center">
-        <IconButton size="sm" icon={<EditIcon />} {...getEditButtonProps()} />
-      </Flex>
-    );
-  }
-
   return (
     <VStack spacing="6">
       <Container>
         <Text fontWeight="extrabold">Email</Text>
-        <Editable
-          defaultValue={userProfile.email}
-          onSubmit={handleEmailSubmit}
-          fontSize="lg"
-          isPreviewFocusable={true}
-          selectAllOnFocus={false}
-        >
-          <EditablePreview />
-          <Input
-            as={EditableInput}
-            ref={email}
-            bgColor="whiteAlpha.900"
-            focusBorderColor="blackAlpha.900"
-          />
-          <EditableControls />
-        </Editable>
+        {isEditingEmail ? (
+          <VStack>
+            <FormControl isInvalid={emailError}>
+              <Input
+                ref={email}
+                bgColor="whiteAlpha.900"
+                focusBorderColor="blackAlpha.900"
+                errorBorderColor="red.300"
+              />
+              {emailError ? (
+                <FormHelperText color="red.300">
+                  Please enter a valid email adress
+                </FormHelperText>
+              ) : null}
+              <ButtonGroup justifyContent="center" size="sm" spacing="4" pt="2">
+                <IconButton
+                  icon={<CheckIcon />}
+                  onClick={handleEmailSubmit}
+                  bgColor="green.200"
+                />
+                <IconButton
+                  icon={<CloseIcon />}
+                  onClick={closeEmailEditable}
+                  bgColor="red.300"
+                  _hover={{ bg: "pink.500" }}
+                />
+              </ButtonGroup>
+            </FormControl>
+          </VStack>
+        ) : (
+          <VStack>
+            <Text>{newEmail ? newEmail : userProfile.email}</Text>
+            <Flex justifyContent="center">
+              <IconButton
+                icon={<EditIcon />}
+                size="sm"
+                onClick={openEmailEditable}
+              />
+            </Flex>
+          </VStack>
+        )}
       </Container>
       <Divider borderColor="blackAlpha.900" />
       <Container>
         <Text fontWeight="extrabold">Username</Text>
-        <Editable
-          defaultValue={userProfile.username}
-          onSubmit={handleUsernameSubmit}
-          fontSize="lg"
-          isPreviewFocusable={true}
-          selectAllOnFocus={false}
-        >
-          <EditablePreview />
-          <Input
-            as={EditableInput}
-            ref={username}
-            bgColor="whiteAlpha.900"
-            focusBorderColor="blackAlpha.900"
-          />
-          <EditableControls />
-        </Editable>
+        {isEditingUsername ? (
+          <VStack>
+            <FormControl isInvalid={usernameError}>
+              <Input
+                ref={username}
+                bgColor="whiteAlpha.900"
+                focusBorderColor="blackAlpha.900"
+              />
+              {usernameError ? (
+                <FormHelperText color="red.300">
+                  Invalid username
+                </FormHelperText>
+              ) : null}
+              <ButtonGroup justifyContent="center" size="sm" spacing="4" pt="2">
+                <IconButton
+                  icon={<CheckIcon />}
+                  onClick={handleUsernameSubmit}
+                  bgColor="green.200"
+                />
+                <IconButton
+                  icon={<CloseIcon />}
+                  onClick={closeUsernameEditable}
+                  bgColor="red.300"
+                  _hover={{ bg: "pink.500" }}
+                />
+              </ButtonGroup>
+            </FormControl>
+          </VStack>
+        ) : (
+          <VStack>
+            <Text>{newUsername ? newUsername : userProfile.username}</Text>
+            <IconButton
+              icon={<EditIcon />}
+              size="sm"
+              onClick={openUsernameEditable}
+            />
+          </VStack>
+        )}
       </Container>
       <Divider borderColor="blackAlpha.900" />
-      {newEmail || newUsername ? (
+      {(newEmail && newEmail !== userProfile.email) ||
+      (newUsername && newUsername !== userProfile.username) ? (
         <Button onClick={updateUser}>Save changes</Button>
       ) : null}
     </VStack>
