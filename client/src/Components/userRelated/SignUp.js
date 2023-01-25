60e871120431d5d53b310259f275feed028f6cf7
petrikoj <100421329+petrikoj@users.noreply.github.com>
2023-01-25 23:02:41 +0100

    WIP error handling

--- client/src/Components/userRelated/SignUp.js
+++ client/src/Components/userRelated/SignUp.js
@@ -6,6 +6,7 @@ import {
   Container,
   Divider,
   FormControl,
+  FormHelperText,
   FormLabel,
   IconButton,
   Image,
@@ -14,17 +15,25 @@ import {
   useToast,
 } from "@chakra-ui/react";
 import { RiUploadCloud2Line } from "react-icons/ri";
-import { isValidEmail, isValidPassword } from "../../utils/validators";
+import {
+  isValidEmail,
+  isValidPassword,
+  isValidUsername,
+} from "../../utils/validators";
 import { useNavigate } from "react-router-dom";
 import { baseURL } from "../../utils/getServerUrl.js";
 
 function SignUp() {
   const [selectedFile, setSelectedFile] = useState(null);
   const [newUser, setNewUser] = useState({});
-  // const [error, setError] = useState(null);
-  const [emailError, setEmailError] = useState(null);
-  const [passwordError, setPasswordError] = useState(null);
-  const [credentialsError, setCredentialsError] = useState(null);
+  const [submitLoading, setSubmitLoading] = useState(false);
+  const [imgLoading, setImgLoading] = useState(false);
+  const [isImg, setIsImg] = useState(false);
+  const [imgError, setImgError] = useState(false);
+  const [usernameError, setUsernameError] = useState(false);
+  const [emailError, setEmailError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+  const [isInput, setIsInput] = useState(true);
   const redirect = useNavigate();
   const toast = useToast();
 
@@ -48,57 +57,180 @@ function SignUp() {
 
   const attachFileHandler = (event) => {
     setSelectedFile(event.target.files[0]);
+    setIsImg(true);
   };
 
   const submitForm = async (event) => {
     event.preventDefault();
     const formData = new FormData();
     formData.append("image", selectedFile);
-
     console.log("selectedFile:", selectedFile);
     console.log("formData:", formData);
-
-    const requestOptions = {
-      method: "POST",
-      body: formData,
-    };
-
-    try {
-      const response = await fetch(
-        baseURL + "/api/users/image-upload",
-        requestOptions
-      );
-      const result = await response.json();
-      console.log("Result:", result);
-      setNewUser({ ...newUser, avatar: result.avatar });
-    } catch (error) {}
+    if (!selectedFile || selectedFile === "") {
+      setImgError(true);
+    } else {
+      setImgError(false);
+      const requestOptions = {
+        method: "POST",
+        body: formData,
+      };
+      try {
+        setImgLoading(true);
+        const response = await fetch(
+          baseURL + "/api/users/image-upload",
+          requestOptions
+        );
+        const result = await response.json();
+        console.log("Result:", result);
+        setNewUser({ ...newUser, avatar: result.avatar });
+        setImgLoading(false);
+      } catch (error) {
+        setImgLoading(false);
+        console.log(error);
+      }
+    }
   };
 
   const registerNewUser = async () => {
-    if (!isValidEmail(newUser.email) && !isValidPassword(newUser.password)) {
-      alert(credentialsError);
-      return;
+    setSubmitLoading(true);
+    // Error handling //
+    /* if (
+      !isValidUsername(newUser.username) &&
+      !isValidEmail(newUser.email) &&
+      !isValidPassword(newUser.password) &&
+      !selectedFile
+    ) {
+      setSubmitLoading(false);
+      setUsernameError(true);
+      setEmailError(true);
+      setPasswordError(true);
+      setImgError(true);
     }
-    if (!isValidEmail(newUser.email)) {
-      alert(emailError);
-      return;
+    if (
+      isValidUsername(newUser.username) &&
+      !isValidEmail(newUser.email) &&
+      !isValidPassword(newUser.password) &&
+      !selectedFile
+    ) {
+      setSubmitLoading(false);
+      setUsernameError(false);
+      setEmailError(true);
+      setPasswordError(true);
+      setImgError(true);
     }
-    if (!isValidPassword(newUser.password)) {
-      alert(passwordError);
-      return;
+    if (
+      isValidUsername(newUser.username) &&
+      isValidPassword(newUser.password) &&
+      !isValidEmail(newUser.email) &&
+      !selectedFile
+    ) {
+      setSubmitLoading(false);
+      setUsernameError(false);
+      setPasswordError(false);
+      setEmailError(true);
+      setImgError(true);
     }
+    if (
+      isValidUsername(newUser.username) &&
+      isValidEmail(newUser.email) &&
+      !isValidPassword(newUser.password) &&
+      !selectedFile
+    ) {
+      setSubmitLoading(false);
+      setUsernameError(false);
+      setEmailError(false);
+      setPasswordError(true);
+      setImgError(true);
+    }
+    if (
+      isValidPassword(newUser.password) &&
+      !isValidUsername(newUser.username) &&
+      !isValidEmail(newUser.email) &&
+      !selectedFile
+    ) {
+      setSubmitLoading(false);
+      setPasswordError(false);
+      setUsernameError(true);
+      setEmailError(true);
+      setImgError(true);
+    }
+    if (
+      isValidUsername(newUser.username) &&
+      isValidPassword(newUser.password) &&
+      !isValidEmail(newUser.email) &&
+      !selectedFile
+    ) {
+      setSubmitLoading(false);
+      setPasswordError(false);
+      setUsernameError(false);
+      setEmailError(true);
+      setImgError(true);
+    }
+    if (
+      isValidUsername(newUser.username) &&
+      isValidEmail(newUser.email) &&
+      isValidPassword(newUser.password) &&
+      !selectedFile
+    ) {
+      setSubmitLoading(false);
+      setUsernameError(false);
+      setEmailError(false);
+      setPasswordError(false);
+      setImgError(true);
+    }
+    if (
+      isValidEmail(newUser.email) &&
+      isValidPassword(newUser.password) &&
+      !isValidUsername(newUser.username) &&
+      !selectedFile
+    ) {
+      setSubmitLoading(false);
+      setUsernameError(true);
+      setImgError(true);
+      setEmailError(false);
+      setPasswordError(false);
+    }
+    if (
+      isValidEmail(newUser.email) &&
+      !isValidPassword(newUser.password) &&
+      !isValidUsername(newUser.username) &&
+      !selectedFile
+    ) {
+      setSubmitLoading(false);
+      setEmailError(false);
+      setUsernameError(true);
+      setPasswordError(true);
+      setImgError(true);
+    }
+    if (
+      !isValidEmail(newUser.email) &&
+      !isValidPassword(newUser.password) &&
+      !isValidUsername(newUser.username) &&
+      isImg
+    ) {
+      setSubmitLoading(false);
+      setImgError(false);
+      setEmailError(true);
+      setUsernameError(true);
+      setPasswordError(true);
+    } */
 
-    if (isValidEmail(newUser.email) && isValidPassword(newUser.password)) {
+    // Handle valid input data //
+    if (
+      isValidUsername(newUser.username) &&
+      isValidEmail(newUser.email) &&
+      isValidPassword(newUser.password) &&
+      selectedFile
+    ) {
+      setUsernameError(false);
+      setEmailError(false);
+      setPasswordError(false);
+      setImgError(false);
       let urlencoded = new URLSearchParams();
       urlencoded.append("username", newUser.username);
       urlencoded.append("email", newUser.email);
       urlencoded.append("password", newUser.password);
-      urlencoded.append(
-        "avatar",
-        newUser.avatar !== ""
-          ? newUser.avatar
-          : "https://www.kindpng.com/imgv/iwoTwxh_transparent-radio-icon-png-headphones-icon-icon-png/"
-      );
+      urlencoded.append("avatar", newUser.avatar);
       const requestOptions = {
         method: "POST",
         body: urlencoded,
@@ -117,22 +249,25 @@ function SignUp() {
           duration: 1500,
           isClosable: true,
         });
+        setSubmitLoading(false);
         redirect("/login");
       } catch (error) {
+        setSubmitLoading(false);
         console.log("Fetch error", error);
       }
+    } else {
+      alert("sry");
+      setSubmitLoading(false);
     }
   };
 
   useEffect(() => {
-    setCredentialsError("Wrong credentials");
-    setEmailError("Wrong E-Mail");
-    setPasswordError("Wrong Password");
-  }, [credentialsError, emailError, passwordError]);
+    console.log("useEffect in SignUp ran");
+  }, []);
 
   return (
     <Container>
-      <FormControl mb="4">
+      <FormControl mb="4" isInvalid={usernameError}>
         <FormLabel>Username</FormLabel>
         <Input
           variant="custom"
@@ -144,8 +279,11 @@ function SignUp() {
           value={newUser.username ? newUser.username : ""}
           onChange={handleChangeHandler}
         />
+        {usernameError ? (
+          <FormHelperText color="red.300">Invalid username</FormHelperText>
+        ) : null}
       </FormControl>
-      <FormControl mb="4">
+      <FormControl mb="4" isInvalid={emailError}>
         <FormLabel>E-Mail</FormLabel>
         <Input
           variant="custom"
@@ -157,8 +295,11 @@ function SignUp() {
           value={newUser.email ? newUser.email : ""}
           onChange={handleChangeHandler}
         />
+        {emailError ? (
+          <FormHelperText color="red.300">Invalid email address</FormHelperText>
+        ) : null}
       </FormControl>
-      <FormControl mb="4">
+      <FormControl mb="4" isInvalid={passwordError}>
         <FormLabel>Password</FormLabel>
         <Input
           variant="custom"
@@ -170,11 +311,14 @@ function SignUp() {
           value={newUser.password ? newUser.password : ""}
           onChange={handleChangeHandler}
         />
+        {passwordError ? (
+          <FormHelperText color="red.300">Invalid password</FormHelperText>
+        ) : null}
       </FormControl>
-      <FormControl mb="8" display="flex" flexDir="column">
+      <FormControl mb="8" display="flex" flexDir="column" isInvalid={imgError}>
         <FormLabel>Profile Picture</FormLabel>
         <Input
-          variant="unstyled"
+          variant="custom"
           type="file"
           accept=".jpg, .jpeg, .png"
           name="image"
@@ -182,6 +326,12 @@ function SignUp() {
           p="2"
           mb="2"
         />
+        {imgError ? (
+          <FormHelperText color="red.300">
+            Please be so kind and provide a profile picture. It can be anything,
+            really.
+          </FormHelperText>
+        ) : null}
         {!newUser.avatar && (
           <Center>
             <IconButton
@@ -189,14 +339,16 @@ function SignUp() {
               w={["80", "40"]}
               leftIcon={<RiUploadCloud2Line />}
               p="2"
+              isLoading={imgLoading ? true : false}
+              isDisabled={!isImg ? true : false}
               onClick={submitForm}
             >
-              <Text>Upload img</Text>
+              <Text>Upload</Text>
             </IconButton>
           </Center>
         )}
       </FormControl>
-
+      {/* Show the upload result */}
       {newUser.avatar && (
         <Center>
           <Image
@@ -210,9 +362,13 @@ function SignUp() {
           />
         </Center>
       )}
-
       <Center>
-        <Button w={["80", "40"]} onClick={registerNewUser}>
+        <Button
+          w={["80", "40"]}
+          isLoading={submitLoading ? true : false}
+          isDisabled={!isInput ? true : false}
+          onClick={registerNewUser}
+        >
           Register
         </Button>
       </Center>
